7bb6b8f91b39ce5d0bd411b0caa64365e5459c96
Luis Riego <luisriego@users.noreply.github.com>
2022-12-12 21:52:56 -0300

    Command Microservices Application, Domain and Infrastructure implementations

--- Banking.Account.Command.Application/obj/Debug/net6.0/Banking.Account.Command.Application.AssemblyInfo.cs
+++ Banking.Account.Command.Application/obj/Debug/net6.0/Banking.Account.Command.Application.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Banking.Account.Command.Domain")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Banking.Account.Command.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Banking.Account.Command.Domain")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Banking.Account.Command.Domain")]
+[assembly: System.Reflection.AssemblyProductAttribute("Banking.Account.Command.Application")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Banking.Account.Command.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
